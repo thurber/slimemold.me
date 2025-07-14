87bee8e7cc03c850e5b9a1232828b40cc2eac63a
GitHub Action <action@github.com>
2025-07-14 03:49:36 +0000

    deploy

---
+++ _app/immutable/chunks/CFb6qcuU.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1tlaxlg)==null?void 0:s.base)??"";var a;const l=((a=globalThis.__sveltekit_1tlaxlg)==null?void 0:a.assets)??t;export{l as a,t as b};
---
+++ _app/immutable/nodes/0.DCgKnjSm.js
@@ -0,0 +1,1 @@
+import{c as n,a}from"../chunks/B7FjXTju.js";import{i}from"../chunks/D9KwifYD.js";import{s as p}from"../chunks/DQg7COx8.js";const s=!0,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:s},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=i(e);p(t,()=>r.children),a(o,e)}export{u as component,f as universal};
