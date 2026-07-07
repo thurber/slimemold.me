8848822dbe3b45a65f7acdbe7bc00c2d5e6dfe7e
thurber <travis.thurber@gmail.com>
2026-07-07 05:23:36 -0700

    cleanup

--- src/lib/removeImage.zsh
+++ src/lib/removeImage.zsh
@@ -6,10 +6,12 @@ out="$me"/../../static/img
 filename=${1##*/}
 basename=${filename%.*}
 
-for style in cyberslime dithernoir dithscii
+for style in cyberslime dithernoir dithscii photo
 do
     rm "$out/$style/lg/$basename.webp"
     rm "$out/$style/sm/$basename.webp"
 done
 
-jq -S --arg basename $basename 'del(.[] | select(. == $basename ))' "$me"/photos.json
+jq -S --arg basename $basename 'del(.[] | select(. == $basename ))' "$me"/photos.json > "$me"/photos.tmp
+jq "sort" "$me"/photos.tmp > "$me"/photos.json
+rm "$me"/photos.tmp
